b406a64f97c5a45171fe98319602c154daba4f93
Emile Latour <latour@ohsu.edu>
2019-07-07 12:14:26 -0700

    Quiet concerns of R CMD check about the "."s

---
+++ R/redcaptools.R
@@ -0,0 +1,28 @@
+#' redcaptools: Misc tools for working with REDCap data in R
+#'
+#' This package contains a some functions that I find handy when working with
+#' REDCap data.
+#'
+#' @examples
+#' # Example usage:
+#' library(redcaptools)
+#'
+#' @docType package
+#' @name redcaptools
+NULL
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+## From Jenny Bryan's googlesheets package
+## From infer package
+## https://github.com/tidymodels/infer/blob/master/R/infer.R
+if (getRversion() >= "2.15.1") {
+  utils::globalVariables(
+    c("."))
+
+}
+
+
+
+
+
+
